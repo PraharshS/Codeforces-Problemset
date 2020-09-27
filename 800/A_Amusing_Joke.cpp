4cbc574a667c314153eb76f03360e3bbf7030dbe
Praharsh Singh <praharsh535@gmail.com>
2020-09-27 11:07:52 +0530

    first commit

---
+++ 800/A_Amusing_Joke.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+
+#define pb push_back
+#define mp make_pair
+#define fr first
+#define sc second
+#define MOD 1e9 + 7
+#define INF 1e9 + 7
+#define len(x) x.size()
+#define min(a, b) ((a) < (b) ? (a) : (b))
+#define max(a, b) ((a) > (b) ? (a) : (b))
+#define min3(a, b, c) min(a, min(b, c))
+#define max3(a, b, c) max(a, max(b, c))
+#define FOR(i, n) for (ll i = 0; i < n; i++)
+#define FORR(i, n) for (ll i = n - 1; i >= 0; i--)
+#define all(v) v.begin(), v.end()
+#define alla(a, n) a, a + n
+#define endl "\n";
+#define fast                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<ll, ll> pll;
+typedef vector<ll> vll;
+typedef vector<int> vi;
+typedef vector<pll> vpll;
+typedef vector<vll> vvll;
+typedef vector<string> vs;
+typedef unordered_map<ll, ll> ump;
+
+int32_t main()
+{
+    fast;
+    string a, b, c;
+    cin >> a >> b >> c;
+    unordered_map<char, int> mp;
+    unordered_map<char, int> mp2;
+
+    for (char i : a)
+    {
+        mp[i]++;
+    }
+    for (char i : b)
+    {
+        mp[i]++;
+    }
+    for (char i : c)
+    {
+        if (mp.find(i) == mp.end())
+        {
+            cout << "NO" << endl;
+            return 0;
+        }
+        mp2[i]++;
+    }
+    for (auto i : mp)
+    {
+        if (i.second != mp2[i.first])
+        {
+            cout << "NO" << endl;
+            return 0;
+        }
+    }
+    cout << "YES" << endl;
+
+    return 0;
+}
